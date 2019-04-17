2cb18eda7728982e8f37cd32059d334259c9610c
Guillermo Valle <guillefix@gmail.com>
2019-04-17 23:48:46 +0100

    layer sweep results

--- batch.sh
+++ batch.sh
@@ -7,10 +7,13 @@
 #SBATCH --time=24:00:00
 
 # set name of job
-#SBATCH -J big_confusion_fc
+#SBATCH -J layer_sweep
+
+#small for 1 gpu, big for 4 or 8
+#SBATCH --partition=small
 
 # set number of GPUs
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:1
 
 # mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
@@ -20,4 +23,4 @@
 
 #Launching the commands within script.sh
 
-/jmain01/apps/docker/tensorflow-batch -v 18.07-py3 -c './meta_script'
+/jmain01/apps/docker/tensorflow-batch -v 18.07-py3 -c './meta_script_layer_sweep'
