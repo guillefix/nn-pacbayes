fixing the weight variance computation code

NN_train.py
@@ -63,8 +63,6 @@ def main(_):
     test_accs = []
     train_accs = []
     weightss = []
-    if network == "cnn":
-        weightss_first_layer = []
     biasess = []
     weights_norms = []
     biases_norms = []
@@ -105,11 +103,10 @@ def main(_):
         print('Test accuracy:', test_acc)
         test_accs.append(test_acc)
         train_accs.append(train_acc)
-        if network == "cnn" or network == "fc":
-            weightss.append(weigths)
-            biasess.append(biases)
-            weights_norms.append(weights_norm)
-            biases_norms.append(biases_norm)
+        weightss.append(weights)
+        biasess.append(biases)
+        weights_norms.append(weights_norm)
+        biases_norms.append(biases_norm)
         iterss.append(model.history.epoch[-1])
         keras.backend.clear_session()
 
@@ -124,10 +121,12 @@ def main(_):
 
     '''PROCESS COLLECTIVE DATA'''
     if rank == 0:
-        weightss = np.stack(weightss,axis=-1)
-        biasess = np.stack(biasess,axis=-1)
-        weights_std = np.mean(np.std(weightss,axis=-1))
-        biases_std = np.mean(np.std(biasess,axis=-1))
+        weightss = np.stack(sum(weightss,[]))
+        biasess = np.stack(sum(biasess,[]))
+        weights_std = np.mean(np.std(weightss,axis=0)).squeeze()
+        biases_std = np.mean(np.std(biasess,axis=0)).squeeze()
+        weights_mean = np.mean(weightss)
+        biases_mean = np.mean(biasess)
         weights_norm_mean = np.mean(weights_norms)
         weights_norm_std = np.std(weights_norms)
         biases_norm_mean = np.mean(biases_norm)
@@ -153,12 +152,12 @@ def main(_):
             file.write("#")
             for key, value in sorted(useful_flags.items()):
                 file.write("{}\t".format(key))
-            file.write("\t".join(["train_acc", "test_error","weights_std","biases_std","weights_norm_mean","weights_norm_std","biases_norm_mean","biases_norm_std","mean_iters","train_acc_std","test_acc_std"]))
+            file.write("\t".join(["train_acc", "test_error","weights_std","biases_std","weights_mean", "biases_mean", "weights_norm_mean","weights_norm_std","biases_norm_mean","biases_norm_std","mean_iters","train_acc_std","test_acc_std"]))
             file.write("\n")
             for key, value in sorted(useful_flags.items()):
                 file.write("{}\t".format(value))
-            file.write("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:d}\t{:.4f}\t{:.4f}\n".format(train_acc, 1-test_acc,weights_std,biases_std,\
-                weights_norm_mean,weights_norm_std,biases_norm_mean,biases_norm_std,int(mean_iters),train_acc_std,test_acc_std)) #normalized to sqrt(input_dim)
+            file.write("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:d}\t{:.4f}\t{:.4f}\n".format(train_acc, 1-test_acc,weights_std,biases_std,\
+                weights_mean,biases_mean,weights_norm_mean,weights_norm_std,biases_norm_mean,biases_norm_std,int(mean_iters),train_acc_std,test_acc_std)) #normalized to sqrt(input_dim)
     else:
         assert test_accs is None
         assert train_accs is None

generate_NN_arch.py
@@ -56,10 +56,10 @@ def main(_):
     input_dim = image_height*image_width*number_channels
     set_session = keras.backend.set_session
 
-    # bias_initializer = keras.initializers.RandomNormal(stddev=1.0)
-    # weight_initializer = keras.initializers.RandomNormal(stddev=1.0/np.sqrt(input_dim))
-    bias_initializer = keras.initializers.Zeros()
-    weight_initializer = keras.initializers.glorot_uniform()
+    bias_initializer = keras.initializers.RandomNormal(stddev=sigmab)
+    weight_initializer = keras.initializers.RandomNormal(stddev=sigmaw/np.sqrt(input_dim))
+    # bias_initializer = keras.initializers.Zeros()
+    # weight_initializer = keras.initializers.glorot_uniform()
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU

utils.py
@@ -173,6 +173,6 @@ def get_rescaled_weights(model):
         else:
             return w*np.sqrt(np.prod(shape[:-1]))
     weights, biases = get_weights(model), get_biases(model)
-    ws = [get_rescaled_weight(w) for w in weights]
-    bs = [get_rescaled_weight(w) for w in biases]
+    ws = np.concatenate([get_rescaled_weight(w).flatten() for w in weights])
+    bs = np.concatenate([get_rescaled_weight(w).flatten() for w in biases])
     return ws, bs