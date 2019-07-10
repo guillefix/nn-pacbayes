3e95b67476a777ea136b96a8805e16eea136f74d
Guillermo Valle <guillefix@gmail.com>
2019-07-10 14:30:32 +0100

    test_mpi

--- test_mpi.py
+++ test_mpi.py
@@ -7,7 +7,7 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 print(rank)
 
-n_gpus=8
+n_gpus=1
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 config.log_device_placement = False  # to log device placement (on which device the operation ran)
