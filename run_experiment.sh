527cc1ea6fc618d4fc9934dbb2cce78dd313f90a
Guillermo Valle <guillefix@gmail.com>
2019-10-09 18:19:54 +0100

    hydra changes

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 echo =========================================================   
-echo Job submitted  date = Mon Oct  7 21:58:05 BST 2019      
+echo Job submitted  date = Tue Oct  8 17:50:09 BST 2019      
 date_start=`date +%s`
 echo $SLURM_JOB_NUM_NODES nodes \( $SMP processes per node \)        
 echo $SLURM_JOB_NUM_NODES hosts used: $SLURM_JOB_NODELIST      
@@ -17,7 +17,7 @@ export MV2_SMP_USE_CMA=0
 
 #which mpirun
 export OMP_NUM_THEADS=1
- nice -n 10 /users/guillefix/nn-pacbayes/./run_experiment --prefix new_arch_sweep_ --m 100 --dataset cifar --network resnet50 --number_layers 4 --training --sigmaw 1.414 --sigmab 0.0 --label_corruption 0 --n_gpus 0 --compute_bound --pooling none --n_samples_repeats 0.025 --number_inits 1 --use_empirical_K
+ nice -n 10 /users/guillefix/nn-pacbayes/./run_experiment --prefix new_arch_sweep_ --m 100 --dataset cifar --network resnet50 --number_layers 4 --training --sigmaw 0.1 --sigmab 0.0 --label_corruption 0 --n_gpus 0 --compute_bound --pooling none --n_samples_repeats 0.25 --number_inits 1 --use_empirical_K
 # If we've been checkpointed
 #if [ -n "${DMTCP_CHECKPOINT_DIR}" ]; then
   if [ -d "${DMTCP_CHECKPOINT_DIR}" ]; then
