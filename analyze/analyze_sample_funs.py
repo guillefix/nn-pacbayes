1d2845244a9a1220ee565a29e511a1c12b630b09
Guillermo Valle <guillefix@gmail.com>
2019-11-08 14:41:08 +0000

    'analyzing sample funs'

--- analyze/analyze_sample_funs.py
+++ analyze/analyze_sample_funs.py
@@ -3,7 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 %matplotlib
 
-d = pd.read_csv("results/index_funs_probs_cnn_sample__mnist_cnn_4_none_0000.txt", header=None, names=["index","fun","ent"],delim_whitespace=True)
+d = pd.read_csv("results/index_funs_probs_cnn_sample__mnist_cnn_4_none_0000_sigmab1.0.txt", header=None, names=["index","fun","ent"],delim_whitespace=True)
+d2 = pd.read_csv("results/index_funs_probs_cnn_sample__mnist_cnn_4_none_0000_sigmab1.0_2.txt", header=None, names=["index","fun","ent"],delim_whitespace=True)
+d = pd.concat([d,d2],axis=0)
 
 num_ones = lambda x: len([c for c in x if c=="1"])
 
@@ -17,6 +19,6 @@ len(hist_data[0])
 plt.bar(hist_data[1][:-1],hist_data[0]/np.sum(hist_data[0]),width=1, log=True)
 plt.xlabel("T")
 plt.ylabel("Probability")
-plt.savefig("T_dist_cnn_sample1e5__mnist_cnn_4_none_0000.png")
+plt.savefig("T_dist_cnn_sample2e5__mnist_cnn_4_none_0000_sigmab1.0.png")
 
 d["ent"].plot.bar(density=True, bins=100, log=True)
