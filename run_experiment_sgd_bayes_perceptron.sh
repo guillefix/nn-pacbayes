1012873d4ebe5bc3ba593c12a455a3de7a0a4432
Guillermo Valle <guillefix@gmail.com>
2019-11-08 20:10:02 +0000

    'meta_script_sgd_bayes_perceptron'

--- run_experiment_sgd_bayes_perceptron.sh
+++ run_experiment_sgd_bayes_perceptron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 echo =========================================================   
-echo Job submitted  date = Thu Nov  7 22:11:07 GMT 2019      
+echo Job submitted  date = Fri Nov  8 15:51:54 GMT 2019      
 date_start=`date +%s`
 echo $SLURM_JOB_NUM_NODES nodes \( $SMP processes per node \)        
 echo $SLURM_JOB_NUM_NODES hosts used: $SLURM_JOB_NODELIST      
@@ -17,7 +17,7 @@ export MV2_SMP_USE_CMA=0
 
 #which mpirun
 export OMP_NUM_THEADS=1
- nice -n 10 /users/guillefix/nn-pacbayes/./run_experiment_sgd_bayes_perceptron --prefix sgd_perceptron_64_512_0_8_sgd_ce_1_ --m 64 --dataset boolean --network fc --number_layers 0 --training --sigmaw 1.0 --sigmab 0.0 --n_gpus 0 --pooling none --loss ce --optimizer sgd --number_inits 600000 --ignore_non_fit --batch_size 8 --layer_width 512 --epochs_after_fit 1 --zero_one --boolfun 00000000000000001111111111111111000000000000000011111111111111110000000000000000111111111111111100000000000000001111111111111110
+ nice -n 10 /users/guillefix/nn-pacbayes/./run_experiment_sgd_bayes_perceptron --prefix sgd_fc_64___8_sgd_ce_1_ --m 64 --dataset boolean --network fc --number_layers 2 --training --sigmaw 1.0 --sigmab 1.0 --n_gpus 0 --pooling none --loss ce --optimizer sgd --number_inits 600000 --ignore_non_fit --batch_size 8 --layer_width 40 --epochs_after_fit 1 --zero_one --boolfun 00110011001100110011001100110011001100110011001100110011001100110011001100110011001100110011001100110011001100110011001100110011
 # If we've been checkpointed
 #if [ -n "${DMTCP_CHECKPOINT_DIR}" ]; then
   if [ -d "${DMTCP_CHECKPOINT_DIR}" ]; then
