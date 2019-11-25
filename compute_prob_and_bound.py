'fixed some things with tensorflow 2.0, and a bug in empirical kernel averaging, specially bad when number of samples was small'

compute_prob_and_bound.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import keras
+#import keras
+from tensorflow import keras
 import pickle
 import os
 
@@ -25,7 +26,7 @@ def main(_):
     config.gpu_options.allow_growth = True
 
     #tf.enable_eager_execution(config=config)
-    set_session = keras.backend.set_session
+    set_session = tf.compat.v1.keras.backend.set_session
     config.log_device_placement = False  # to log device placement (on which device the operation ran)
     sess = tf.compat.v1.Session(config=config)
     set_session(sess)  # set this TensorFlow session as the default session for Keras
@@ -44,7 +45,9 @@ def main(_):
 
     print("compute probability and bound", network, dataset)
 
-    K = load_kernel(FLAGS)
+    K_pre = load_kernel(FLAGS)
+    print(K_pre)
+    K = kernel_mult*K_pre
 
     #finding log marginal likelihood of data
     if using_EP:
@@ -114,6 +117,7 @@ if __name__ == '__main__':
     f.DEFINE_boolean('using_MC', False, "Whether to use Monte Carlo method for computing probability")
     f.DEFINE_integer('num_post_samples', int(1e5), "Number of approximate EP posterior samples in importance-sampling-based Monte Carlo estimation of marginal likelihood")
     f.DEFINE_float('cov_mult', 1.0, "Factor by which to multiply the variance of the approximate posterior, to focus the importance sampling more in the non-zero likelihood region, at the risk of biasing away from true posterior.")
+    f.DEFINE_float('kernel_mult', 1.0, "Factor by which to multiply the kernel before computing approximate marginal likelihood")
     f.DEFINE_float('mean_mult', 1.0, "Factor by which to multiply the mean of the approximate posterior, to focus the importance sampling more in the non-zero likelihood region, at the risk of biasing away from true posterior.")
 
     tf.compat.v1.app.run()

generate_inputs_sample.py
@@ -204,8 +204,10 @@ def main(_):
         max2 = torch.max(test_images).item()
         print("maxs", max1,max2)
         max_val = max(max1,max2)
-        train_images  = train_images.numpy().astype(np.float32)*255.0/max_val
-        test_images = test_images.numpy().astype(np.float32)*255.0/max_val
+        #train_images  = train_images.numpy().astype(np.float32)*255.0/max_val
+        #test_images = test_images.numpy().astype(np.float32)*255.0/max_val
+        train_images  = train_images.numpy().astype(np.uint8)
+        test_images = test_images.numpy().astype(np.uint8)
 
 
         if oversampling:
@@ -291,12 +293,12 @@ def main(_):
                     #print(train_images.shape)
             if network in ["cnn","fc"]:
                 #normalize the images pixels to be in [0,1]
-                train_images = train_images.astype(np.float32)/255.0
+                train_images = train_images.astype(np.float32)/max1
                 if training:
-                    test_images = test_images.astype(np.float32)/255.0
+                    test_images = test_images.astype(np.float32)/max1
             else:
                 #note that the transformation to PIL and back to Tensor normalizes the image pixels to be in [0,1]
-                assert train_images.dtype == "uint8" #otherwise ToPILImage wants the input to be NCHW. wtff
+                assert train_images.dtype == "uint8" #otherwise ToPILImage either fails or wants the input to be NCHW. wtff
                 train_images = np.stack([d.transform(image) for image in train_images])
                 train_images = np.transpose(train_images,(0,2,3,1)) # this is because the pytorch transform changes it to NCHW for some reason :P
                 if training:
@@ -431,7 +433,7 @@ def main(_):
         test_ys = np.array([process_labels(label,label_corruption,threshold,zero_one=True,binarized=binarized) for label in test_labels])
 
         '''SAVING DATA SAMPLES'''
-        print(ys)
+        #print(ys)
         save_data(train_images,ys,test_images,test_ys,FLAGS)
     else:
         test_images = test_ys = []

nngp_kernel/empirical_kernel.py
@@ -72,6 +72,9 @@ def empirical_K(arch_json_string, data, number_samples,sigmaw=1.0,sigmab=1.0,n_g
             reset_weights(model, initial_weights, are_norm, sigmaw, sigmab)
 
         X = np.squeeze(func(data))
+        print("X",X)
+        if len(X.shape)==1:
+            X = np.expand_dims(X,0)
         covs += (sigmaw**2/X.shape[1])*np.matmul(X,X.T)+(sigmab**2)*np.ones((X.shape[0],X.shape[0]))
         #outputs = model.predict(data)
         #print(outputs)
@@ -91,7 +94,7 @@ def empirical_K(arch_json_string, data, number_samples,sigmaw=1.0,sigmab=1.0,n_g
     print("--- %s seconds ---" % (time.time() - start_time))
 
     #fs = comm.gather(fs,root=0)
-    covs = comm.gather(covs/len(tasks),root=0)
+    covs = comm.gather(covs,root=0)
 
     if rank == 0:
         #fs = sum(fs, [])
@@ -100,6 +103,6 @@ def empirical_K(arch_json_string, data, number_samples,sigmaw=1.0,sigmab=1.0,n_g
         #fs = np.array(fs)
         #fs = np.squeeze(fs)
         #return np.cov(fs.T)
-        return np.mean(covs,axis=0)
+        return np.sum(covs,axis=0)/number_samples
     else:
         return None