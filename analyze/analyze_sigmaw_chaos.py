53217b0508fac53c1cb26e7a983c17286663530a
Guillermo Valle <guillefix@gmail.com>
2020-05-28 16:19:28 +0200

    'analyzing chaos'

--- analyze/analyze_sigmaw_chaos.py
+++ analyze/analyze_sigmaw_chaos.py
@@ -22,17 +22,27 @@ for i,sigmaw in enumerate(sigmaws):
     t = reduced_training_results[reduced_training_results["sigmaw"]==sigmaw]
     b = reduced_bounds[reduced_bounds["sigmaw"]==sigmaw]
     c=cmap(1.0*i/len(sigmaws))
-    plt.plot(t["number_layers"],t["test_error"], '-', c=c, label=sigmaw)
-    plt.plot(b["number_layers"],b["bound"], '--', c=c, label=sigmaw)
+    plt.plot(t["number_layers"],t["test_error"], '-', c=c, label=str(sigmaw)+" (SGD)")
+    plt.plot(b["number_layers"],b["bound"], '--', c=c, label=str(sigmaw)+" (PB)")
 
-plt.legend(bbox_to_anchor=(1.12, 1.0))
+plt.legend(bbox_to_anchor=(1.0, 1.1),fontsize=8)
+plt.subplots_adjust(right=0.8)
+plt.xlabel("Number of layers")
+plt.ylabel("Generalization error")
+plt.savefig("error_sgd_pacbayes_vs_layers_mnist10k_fc.png")
 
-for i,number_layers in enumerate(number_layerss):
+for i,number_layers in enumerate(sorted(number_layerss)):
     t = reduced_training_results[reduced_training_results["number_layers"]==number_layers]
     b = reduced_bounds[reduced_bounds["number_layers"]==number_layers]
     c=cmap(1.0*i/len(number_layerss))
-    plt.plot(t["sigmaw"],t["test_error"], '-', c=c)
-    plt.plot(b["sigmaw"],b["bound"], '--', c=c)
+    plt.plot(t["sigmaw"],t["test_error"], '-', c=c, label=str(number_layers)+" (SGD)")
+    plt.plot(b["sigmaw"],b["bound"], '--', c=c, label=str(number_layers)+" (PB)")
+
+plt.legend(bbox_to_anchor=(1.0, 1.0),fontsize=10)
+plt.subplots_adjust(right=0.8)
+plt.xlabel("$\sigma_w$")
+plt.ylabel("Generalization error")
+plt.savefig("error_sgd_pacbayes_vs_sigmaw_mnist10k_fc.png")
 
 ######
 
